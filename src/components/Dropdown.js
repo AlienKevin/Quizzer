b7806706dd17fd5c281c7b2120ce0d0ce75cfacf
Kevin Li <kevinli020508@gmail.com>
2019-05-06 06:21:26 -0400

    Control dropdown textfield value instead of placeholder

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -14,14 +14,14 @@ class Dropdown extends Component {
     super(props);
     this.state = {
       selected: 0,
-      options: ["Option 1"]
+      options: [""]
     };
     this.classes = props.classes;
   }
 
   handleAddOption = () => {
     this.setState(prevState => ({
-      options: [...prevState.options, `Option ${prevState.options.length + 1}`]
+      options: [...prevState.options, ""]
     }));
   };
 
@@ -29,6 +29,19 @@ class Dropdown extends Component {
     this.setState(prevState => {
       // remove the options
       prevState.options.splice(index, 1);
+      console.log(prevState.options);
+      return {
+        options: prevState.options
+      };
+    });
+  };
+
+  updateOptions = event => {
+    const target = event.target;
+    const index = target.id;
+    this.setState(prevState => {
+      prevState.options[index] = target.value;
+      console.log(prevState.options);
       return {
         options: prevState.options
       };
@@ -49,19 +62,27 @@ class Dropdown extends Component {
               {index + "."}
             </Typography>
           );
-          let dropdown = this.state.options.map((option, index) => (
-            <ListItem>
-              <ListNumber index={index + 1} />
-              <TextField placeholder={option} />
-              {this.state.options.length > 1 && (
-                <IconButton
-                  onClick={index => this.handleCloseOptionButton(index)}
-                >
-                  <CloseIcon />
-                </IconButton>
-              )}
-            </ListItem>
-          ));
+          let dropdown = this.state.options.map((option, index) => {
+            console.log("option: " + option);
+            return (
+              <ListItem>
+                <ListNumber index={index + 1} />
+                <TextField
+                  id={index + ""}
+                  placeholder={`Option ${index + 1}`}
+                  value={option}
+                  onChange={this.updateOptions}
+                />
+                {this.state.options.length > 1 && (
+                  <IconButton
+                    onClick={() => this.handleCloseOptionButton(index)}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                )}
+              </ListItem>
+            );
+          });
           dropdown.push(
             <ListItem onClick={this.handleAddOption}>
               <ListNumber index={this.state.options.length + 1} />
