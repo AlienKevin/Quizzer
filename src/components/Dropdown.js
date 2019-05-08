6f90b609a519a46b09c66c37ee0df26801860bed
Kevin Li <kevinli020508@gmail.com>
2019-05-08 12:07:28 -0400

    Remove styling in Checkbox creator

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
-import { TextField, List, ListItem, Typography, IconButton } from "material-ui";
-import CloseIcon from "material-ui-icons/Close";
+import { Typography } from "material-ui";
 import { withStyles } from "material-ui/styles";
+import SelectionCreator from "./SelectionCreator";
 
 const styles = theme => ({
-  listNumber: {
+  ListNumber: {
     marginRight: theme.spacing.unit
   }
 });
@@ -12,94 +12,16 @@ const styles = theme => ({
 class Dropdown extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      selected: 0,
-      options: [""]
-    };
     this.classes = props.classes;
   }
-
-  handleAddOption = () => {
-    this.setState(prevState => ({
-      options: [...prevState.options, ""]
-    }));
-  };
-
-  handleCloseOptionButton = index => {
-    this.setState(prevState => {
-      // remove the options
-      prevState.options.splice(index, 1);
-      console.log(prevState.options);
-      return {
-        options: prevState.options
-      };
-    });
-  };
-
-  updateOptions = event => {
-    const target = event.target;
-    const index = target.id;
-    this.setState(prevState => {
-      prevState.options[index] = target.value;
-      console.log(prevState.options);
-      return {
-        options: prevState.options
-      };
-    });
-  };
-
   render() {
-    return (
-      <List>
-        {(() => {
-          console.log("dropdowning...");
-          const ListNumber = ({ index }) => (
-            <Typography
-              className={this.classes.listNumber}
-              variant="body2"
-              color="textSecondary"
-            >
-              {index + "."}
-            </Typography>
-          );
-          console.log(this.state.options);
-          let dropdown = this.state.options.map((option, index) => {
-            console.log("option: " + option);
-            return (
-              <ListItem>
-                <ListNumber index={index + 1} />
-                <TextField
-                  id={index + ""}
-                  placeholder={`Option ${index + 1}`}
-                  value={option}
-                  onChange={this.updateOptions}
-                />
-                {this.state.options.length > 1 && (
-                  <IconButton
-                    onClick={() => this.handleCloseOptionButton(index)}
-                  >
-                    <CloseIcon />
-                  </IconButton>
-                )}
-              </ListItem>
-            );
-          });
-          dropdown.push(
-            <ListItem onClick={this.handleAddOption}>
-              <ListNumber index={this.state.options.length + 1} />
-              <TextField
-                placeholder={"Add option"}
-                InputProps={{
-                  readOnly: true
-                }}
-                value=""
-              />
-            </ListItem>
-          );
-          return dropdown;
-        })()}
-      </List>
+    const ListNumber = ({ index }) => (
+      <Typography variant="body2" color="textSecondary" className="ListNumber">
+        {index + "."}
+      </Typography>
     );
+    return <SelectionCreator ListMarker={withStyles(styles)(ListNumber)} />;
   }
 }
-export default withStyles(styles)(Dropdown);
+
+export default Dropdown;
