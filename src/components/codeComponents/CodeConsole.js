023b576798a2da473ff045c41eeafca6d505e172
Kevin Li <kevinli020508@gmail.com>
2019-05-17 16:15:42 -0400

    Show errors in CodeConsole

--- src/components/codeComponents/CodeConsole.js
+++ src/components/codeComponents/CodeConsole.js
@@ -4,8 +4,15 @@ import { Controlled as CodeMirror } from "react-codemirror2";
 function CodeConsole(props) {
   console.log("CodeConsole props: ", props);
   const stdout = props.output.stdout;
-  console.log("stdout: " + stdout);
-  return <CodeMirror value={stdout} />;
+  // console.log("stdout: " + stdout);
+  const stderr = props.output.stderr;
+  const error = props.output.error;
+
+  let value =
+    (stdout ? stdout + "\n" : "") +
+    (stderr ? stderr + "\n" : "") +
+    (error ? error : "");
+  return <CodeMirror value={value} />;
 }
 
 export default CodeConsole;
